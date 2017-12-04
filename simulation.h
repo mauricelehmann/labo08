0f224ffb9577d1f7ebbe35df6eacc5637db7f26a
mauricelehmann <m.lehmann94@gmail.com>
2017-12-04 21:56:09 +0100

    meme chose ici

--- simulation.h
+++ simulation.h
@@ -1,20 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   simulation.h
- * Author: mle
- *
- * Created on 4. décembre 2017, 11:45
- */
-
 #ifndef SIMULATION_H
 #define SIMULATION_H
 
+//#include "aleatoire.h"
+#include <random>
+#include <ctime>
 
+#include <iostream>
+#include <iomanip>
+
+
+using namespace std ; 
+
+
+
+void initialisationPosition( int& positionX , int& positionY  , int& nbMouvement );
+void avancerCase( int& positionX , int& positionY ) ;
+void controleCase( int positionX , int positionY , int& status );
+void lancerSimulation( int& positionX , int& positionY , int& status , int& nbMouvement , int map[][5] , int tailleMap );
 
 #endif /* SIMULATION_H */
 
